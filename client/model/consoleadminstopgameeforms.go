65e98a7122765fec3e0425e5e0b514cd35cd6cc3
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-06-09 17:10:51 +0800

    feat: update

---
+++ client/model/consoleadminstopgameeforms.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminStopGameeForms struct {
+    AccountId string `json:"AccountId"`
+    GameId string `json:"GameId"`
+    AppKey string `json:"appKey"`
+    GameSession string `json:"GameSession"`
+}
