d910f80dbc9974ce1dc2c7c1d6a1faeb98194f32
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-06-09 17:10:46 +0800

    feat: update

---
+++ client/model/consoleadminlistgamessssresultmodeldatalist.go
@@ -0,0 +1,13 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminListGamessssResultModelDataList struct {
+    GameName string `json:"GameName,omitempty"`
+    PlatformType int32 `json:"PlatformType,omitempty"`
+    GameId string `json:"GameId,omitempty"`
+}
